4e4a7d10c13c3a91945a88396a14e75ce8be692f
Kevin Brian Bader <kevin.bader96@gmail.com>
2024-04-26 19:20:16 -0700

    TS Migration

--- .github/scripts/issue-comment-edit.ts
+++ .github/scripts/issue-comment-edit.ts
@@ -1,7 +1,8 @@
 // Import GitHub toolkit and Octokit REST client
-const { context, getOctokit } = require('@actions/github');
-const InitOpenAI = require('openai');
+import { context, getOctokit } from '@actions/github';
+import InitOpenAI from 'openai';
 
+// @ts-expect-error - process is not imported
 const OpenAI = new InitOpenAI({apiKey: process.env.OPENAI_API_KEY});
 
 /**
@@ -10,22 +11,23 @@ const OpenAI = new InitOpenAI({apiKey: process.env.OPENAI_API_KEY});
  * @param {*} labelNames - String array of label names to check for, ex. ['Help Wanted', "External"]
  * @returns {Promise<false | undefined>}
  */
-async function handleIssueCommentCreated(octokit, labelNames) {
+async function handleIssueCommentEdited(octokit, labelNames) {
     const payload = context.payload;
+    // @ts-expect-error - process is not imported
     const OPENAI_ASSISTANT_ID = process.env.OPENAI_ASSISTANT_ID;
-
+    
     // check if the issue is opened and the has all passed labels
     if (
-        payload.issue.state === 'open' &&
-        labelNames.every(labelName => payload.issue.labels.some(issueLabel => issueLabel.name === labelName))
+        payload.issue?.state === 'open' &&
+        labelNames.every(labelName => payload.issue?.labels.some(issueLabel => issueLabel.name === labelName))
     ) {
         if (!OPENAI_ASSISTANT_ID) {
             console.log('OPENAI_ASSISTANT_ID missing from the environment variables');
             return;
         }
-
-        // 1, check if comment is proposal and if proposal template is followed
-        const content = `I NEED HELP WITH CASE (1.), CHECK IF COMMENT IS PROPOSAL AND IF TEMPLATE IS FOLLOWED AS PER INSTRUCTIONS. IT IS MANDATORY THAT YOU RESPOND ONLY WITH "NO_ACTION" IN CASE THE COMMENT IS NOT A PROPOSAL. Comment content: ${payload.comment.body}`;
+        
+        // You need to adapt this part to fit the Edit Use Case as in the original function
+        const content = `I NEED HELP WITH CASE (2.) WHEN A USER THAT POSTED AN INITIAL PROPOSAL OR COMMENT (UNEDITED) THEN EDITS THE COMMENT - WE NEED TO CLASSIFY THE COMMENT BASED IN THE GIVEN INSTRUCTIONS AND IF TEMPLATE IS FOLLOWED AS PER INSTRUCTIONS. IT IS MANDATORY THAT YOU RESPOND ONLY WITH "NO_ACTION" IN CASE THE COMMENT IS NOT A PROPOSAL. \n\nPrevious comment content: ${payload.changes.body?.from}.\n\nEdited comment content: ${payload.comment?.body}`;
 
         // create thread with first user message and run it
         const createAndRunResponse = await OpenAI.beta.threads.createAndRun({
@@ -38,11 +40,12 @@ async function handleIssueCommentCreated(octokit, labelNames) {
         // poll for run completion
         const intervalID = setInterval(() => {
             OpenAI.beta.threads.runs.retrieve(createAndRunResponse.thread_id, createAndRunResponse.id).then(run => {
-                // return if run is not completed
+                // return if run is not completed yet
                 if (run.status !== "completed") {
+                    console.log('issue_comment.edited - run pending completion');
                     return;
                 }
-
+    
                 // get assistant response
                 OpenAI.beta.threads.messages.list(createAndRunResponse.thread_id).then(threadMessages => {
                     // list thread messages content
@@ -50,12 +53,12 @@ async function handleIssueCommentCreated(octokit, labelNames) {
                         // @ts-ignore - we do have text value in content[0] but typescript doesn't know that
                         // this is a 'openai' package type issue
                         let assistantResponse = message.content?.[index]?.text?.value;
-                        console.log('issue_comment.created - assistantResponse', assistantResponse);
-
+                        console.log('issue_comment.edited - assistantResponse', assistantResponse);
+        
                         if (!assistantResponse) {
-                            return console.log('issue_comment.created - assistantResponse is empty');
+                            return console.log('issue_comment.edited - assistantResponse is empty');
                         }
-
+        
                         // check if assistant response is either NO_ACTION or "NO_ACTION" strings
                         // as sometimes the assistant response varies
                         const isNoAction = assistantResponse === 'NO_ACTION' || assistantResponse === '"NO_ACTION"';
@@ -64,39 +67,35 @@ async function handleIssueCommentCreated(octokit, labelNames) {
                             if (threadMessages.data?.[index]?.role === 'user')  {
                                 return;
                             }
-                            return console.log('issue_comment.created - NO_ACTION');
+                            return console.log('issue_comment.edited - NO_ACTION');
                         }
-
-                        // if the assistant responded with no action but there's some context in the response
-                        if (assistantResponse.includes('[NO_ACTION]')) {
-                            // extract the text after [NO_ACTION] from assistantResponse since this is a
+        
+                        // edit comment if assistant detected substantial changes and if the comment was not edited already by the bot
+                        if (assistantResponse.includes('[EDIT_COMMENT]') && !payload.comment?.body.includes('Edited by **proposal-police**')) {
+                            // extract the text after [EDIT_COMMENT] from assistantResponse since this is a
                             // bot related action keyword
-                            const noActionContext = assistantResponse.split('[NO_ACTION] ')?.[1]?.replace('"', '');
-                            console.log('issue_comment.created - [NO_ACTION] w/ context: ', noActionContext);
-                            return;
+                            let extractedNotice = assistantResponse.split('[EDIT_COMMENT] ')?.[1]?.replace('"', '');
+                            // format the github's updated_at like: 2024-01-24 13:15:24 UTC not 2024-01-28 18:18:28.000 UTC
+                            const date = new Date(payload.comment?.updated_at);
+                            const formattedDate = date.toISOString()?.split('.')?.[0]?.replace('T', ' ') + ' UTC';
+                            extractedNotice = extractedNotice.replace('{updated_timestamp}', formattedDate);
+            
+                            console.log(`issue_comment.edited - proposal-police edits comment: ${payload.comment?.id}`);
+                            return octokit.issues.updateComment({
+                                ...context.repo,
+                                comment_id: payload.comment?.id,
+                                body: `${extractedNotice}\n\n` + payload.comment?.body,
+                            });
                         }
-                        // replace {user} from response template with @username
-                        assistantResponse = assistantResponse.replace('{user}', `@${payload.comment.user.login}`);
-                        // replace {proposalLink} from response template with the link to the comment
-                        assistantResponse = assistantResponse.replace('{proposalLink}', payload.comment.html_url);
-
-                        // remove any double quotes from the final comment because sometimes the assistant's
-                        // response contains double quotes / sometimes it doesn't
-                        assistantResponse = assistantResponse.replace('"', '');
-                        // create a comment with the assistant's response
-                        console.log('issue_comment.created - proposal-police posts comment');
-                        return octokit.issues.createComment({
-                            ...context.repo,
-                            issue_number: payload.issue.number,
-                            body: assistantResponse
-                        });
+        
+                        return false;
                     });
                 }).catch(err => console.log('threads.messages.list - err', err));
-
+    
                 // stop polling
                 clearInterval(intervalID);
             }).catch(err => console.log('threads.runs.retrieve - err', err));
-        
+            
             // increment count for every threads.runs.retrieve call
             count++;
             console.log('threads.runs.retrieve - called:', count);
@@ -107,13 +106,14 @@ async function handleIssueCommentCreated(octokit, labelNames) {
     return false;
 }
 
-// Main function to process the workflow event
 async function run() {
+    // @ts-expect-error - process is not imported
     const octokit = getOctokit(process.env.GITHUB_TOKEN);
-    await handleIssueCommentCreated(octokit, ['Help Wanted']);
+    await handleIssueCommentEdited(octokit, ['Help Wanted']);
 }
 
 run().catch(error => {
     console.error(error);
+    // @ts-expect-error - process is not imported
     process.exit(1);
 });
