6aa77fae7b22467a601db43e349534a0a69ecbde
Kevin Brian Bader <kevin.bader96@gmail.com>
2024-06-22 15:22:37 -0700

    debugging message content 2.0

--- .github/scripts/libs/OpenAIUtils.ts
+++ .github/scripts/libs/OpenAIUtils.ts
@@ -59,7 +59,7 @@ class OpenAIUtils {
                 }
                 console.log('message.content:', message.content);
                 response += message.content
-                    .map((contentBlock) => this.isTextContentBlock(contentBlock))
+                    .map((contentBlock) => this.isTextContentBlock(contentBlock) && contentBlock.text.value)
                     .join('\n')
                     .trim();
                 console.log('Parsed assistant response:', response);
