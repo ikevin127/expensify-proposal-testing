069e9f0c0818d7baa86f3261f64eac4e94cee888
Kevin Brian Bader <kevin.bader96@gmail.com>
2024-06-22 14:22:51 -0700

    bump to latest refactoring

--- .github/scripts/libs/CONST.ts
+++ .github/scripts/libs/CONST.ts
@@ -6,8 +6,17 @@ const CONST = {
         CREATED: 'created',
         EDIT: 'edited',
     },
+    EVENTS: {
+        ISSUE_COMMENT: 'issue_comment',
+    },
+    OPENAI_ROLES: {
+        USER: 'user',
+        ASSISTANT: 'assistant',
+    },
     NO_ACTION: 'NO_ACTION',
     OPENAI_POLL_RATE: 1500,
+    OPENAI_POLL_TIMEOUT: 90000,
+    OPENAI_THREAD_COMPLETED: 'completed',
 } as const;
 
 export default CONST;
