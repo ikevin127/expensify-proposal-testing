78828a5623f69f67ed0abd56d03f6bf6e9efa822
Kevin Brian Bader <kevin.bader96@gmail.com>
2025-08-01 17:34:17 -0700

    OK

--- .github/actions/javascript/postTestCoverageComment/postTestCoverageComment.js
+++ .github/actions/javascript/postTestCoverageComment/postTestCoverageComment.js
@@ -113,9 +113,6 @@ function generateCoverageData(coverage, changedFiles, baseCoverage) {
         };
     }
 
-    console.log('DEBUG: Changed files:', changedFiles);
-    console.log('DEBUG: Coverage keys:', Object.keys(coverage).filter(k => k !== 'total'));
-
     return result;
 }
 
