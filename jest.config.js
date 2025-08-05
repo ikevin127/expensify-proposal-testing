d90f85684f4fa3809e4de18608c44d5167a9eb98
Kevin Brian Bader <kevin.bader96@gmail.com>
2025-08-05 16:43:43 -0700

    completed coverage

--- jest.config.js
+++ jest.config.js
@@ -10,25 +10,19 @@ export default {
       useESM: true,
     }],
   },
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx,js,jsx}',
-    '!src/**/*.d.ts',
-    '!src/**/*.test.{ts,tsx,js,jsx}',
-    '!src/**/*.spec.{ts,tsx,js,jsx}',
-  ],
-  coverageReporters: ['lcov'],
   testMatch: [
     '**/__tests__/**/*.(ts|tsx|js)',
     '**/*.(test|spec).(ts|tsx|js)'
   ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov'],
   reporters: [
-    'default',
-    ['jest-junit', {
-      classNameTemplate: '{filepath}',
-      titleTemplate: '{title}',
-      ancestorSeparator: ' › ',
-      usePathForSuiteName: true
-    }]
+      'default',
+      ['jest-junit', {
+          classNameTemplate: '{filepath}',
+          titleTemplate: '{title}',
+          ancestorSeparator: ' › ',
+          usePathForSuiteName: true
+      }]
   ],
 };
